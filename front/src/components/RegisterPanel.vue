128b0abcd5d206d017ae7119949fb6d2ef50623c
danchin <danchin01@163.com>
2023-03-28 18:04:44 +0800

    登录/注册页面完成

--- front/src/components/RegisterPanel.vue
+++ front/src/components/RegisterPanel.vue
@@ -6,24 +6,27 @@
       label-width="120px"
       class="register"
       >
-      <el-form-item label="Password" prop="pass">
-      <el-input class="input" type="password" autocomplete="off" />
+      <el-form-item>
+      <div class="title-1" style="font-size: small; margin-right: 20px; color:#4b4b4b;">还没有账号❓</div>
+      <div class="title-2" style="font-size: medium; font-weight: 700; color:#4b4b4b">现在加入barley-club🍺</div>
       </el-form-item>
-      <el-form-item label="Confirm" prop="checkPass">
+      <el-form-item label="Phone" prop="checkPass">
       <el-input
-        type="password"
-        autocomplete="off"
       />
       </el-form-item>
-      <el-form-item label="Age" prop="age">
+      <el-form-item label="Password" prop="pass">
+      <el-input type="password" autocomplete="off" />
+      </el-form-item>
+      
+      <el-form-item label="Username" prop="age">
       <el-input/>
       </el-form-item>
 
       <el-form-item>
-      <el-button type="primary"
+      <el-button type="primary" style="border-radius: 100px;"
         >Submit</el-button
       >
-      <el-button >Reset</el-button>
+      <el-button style="border-radius: 100px;">Reset</el-button>
       </el-form-item>
       </el-form>
     </div>
@@ -32,15 +35,25 @@
 
 <script>
 export default {
+  setup() {
+    
+  }
 
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .register-panel {
-  width: 500px;
+  transition: all 0.2s;
+  width: 400px;
   background-color: #f5f5f5;
-  padding: 20px;
+  padding: 20px 60px 20px 2px;
+  margin: 40px;
+  border-radius: 10px;
+}
+
+.register-panel:focus-within{
+  box-shadow: 0 2px 4px 3px #fee5cf;
 }
 
 
